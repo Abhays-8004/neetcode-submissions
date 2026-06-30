6899742a6c46b6e44f1bfe8470a0cc91e4e11f7a
Abhays-8004 <abhaysonwani83@gmail.com>
2026-06-30 10:27:21 +0530

    Add: meeting-schedule - submission-3

---
+++ Data Structures & Algorithms/meeting-schedule/submission-3.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](const Interval& a, const Interval& b) {
+    return a.start < b.start; // Sort in ascending order of start times
+});
+        for(int i = 1;i<intervals.size();i++){
+            if( intervals[i-1].start<=intervals[i].start&&intervals[i].start<intervals[i-1].end){
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+};
